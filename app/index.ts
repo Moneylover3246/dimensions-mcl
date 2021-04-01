d1c4ebcf8c85915e8e55da354fc2683ec605af85
Moneylover3246 <66443019+Moneylover3246@users.noreply.github.com>
2021-04-01 15:51:04 -0400

    Add pc client set

--- app/index.ts
+++ app/index.ts
@@ -1,47 +1,28 @@
-import ListenServer from "dimensions/listenserver";
-import Extension from "dimensions/extension";
-import PriorPacketHandler from "./priorpackethandler";
-import Client from "dimensions/client";
-import { Socket } from "net";
-import PostPacketHandler from "./postpackethandler";
-
-export const MOBILE_SERVER_ID = 16;
-export const PC_SERVER_ID = 255;
-export const MAX_CLIENT_ID = 15;
-export const FAKED_CLIENT_ID = 15;
-export const PACKET_LEN_BYTES = 2;
-export const PACKET_TYPE_BYTES = 1;
-export const PACKET_HEADER_BYTES = PACKET_LEN_BYTES + PACKET_TYPE_BYTES;
-
-class MobileCompatibilityLayer implements Extension {
-    public name: string;
-    public version: string;
-    public author: string;
-    public reloadable: boolean;
-    public priorPacketHandlers: PriorPacketHandler;
-    public postPacketHandlers: PostPacketHandler;
-    public listenServers: { [name: string]: ListenServer };
-    public clients: Set<Client> = new Set<Client>();
-    public realId: Map<Client, number> = new Map<Client, number>(); // used when server tells mobile client their id > MAX_CLIENT_ID
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PACKET_HEADER_BYTES = exports.PACKET_TYPE_BYTES = exports.PACKET_LEN_BYTES = void 0;
+const priorpackethandler_1 = require("./priorpackethandler");
+const postpackethandler_1 = require("./postpackethandler");
+exports.PACKET_LEN_BYTES = 2;
+exports.PACKET_TYPE_BYTES = 1;
+exports.PACKET_HEADER_BYTES = exports.PACKET_LEN_BYTES + exports.PACKET_TYPE_BYTES;
+class MobileCompatibilityLayer {
     constructor() {
-        this.name = "Mobile Compatibility Layer";
-        this.version = "v1.1";
+        this.clients = new Set();
+        this.pcClients = new Set();
+        this.name = "Mobile Compatibility Layer 1.4.0.5 -> 1.4.1.1. With iOS support!";
+        this.version = "v1.4";
         this.author = "popstarfreas";
         this.reloadable = false;
-        this.priorPacketHandlers = new PriorPacketHandler(this);
-        this.postPacketHandlers = new PostPacketHandler(this);
+        this.priorPacketHandlers = new priorpackethandler_1.default(this);
+        this.postPacketHandlers = new postpackethandler_1.default(this);
     }
-
-    public setListenServers(listenServers: { [name: string]: ListenServer }): void {
+    setListenServers(listenServers) {
         this.listenServers = listenServers;
     }
-
-    public socketClosePostHandler(socket: Socket, client: Client) {
+    socketClosePostHandler(_socket, client) {
         this.clients.delete(client);
-        this.realId.delete(client);
+        this.pcClients.delete(client);
     }
-
 }
-
-export default MobileCompatibilityLayer;
+exports.default = MobileCompatibilityLayer;
